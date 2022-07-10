5ce0c879e44093fd4d129638b24264b04e85b265
kenta <c.kenta2018@gmail.com>
2022-07-11 01:44:43 +0900

    対前訪れた人　完成

--- src/client.java
+++ src/client.java
@@ -17,9 +17,13 @@ public class client extends JFrame implements ActionListener{
         frame.setTitle("ジャンケンゲーム");
     }
     //変数の宣言
+    public int myhand;
+    public String name;
     JPanel card1;
     JPanel card2;
-    public int myhand;
+    JPanel cardRegister;
+    JTextField textField;
+    JButton buttonOk;
     JLabel handLabel =new JLabel();
     JLabel judgeLabel =new JLabel();
     JButton button1;
@@ -28,36 +32,56 @@ public class client extends JFrame implements ActionListener{
     JButton againButton;
 
     client(){
+        //ユーザーネーム登録カード
+        cardRegister = new JPanel();
+        cardRegister.setBackground(new Color(240,248,255));
+        JLabel registerLabel = new JLabel("ユーザネームを登録してください");
+        registerLabel.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 25));
+        registerLabel.setPreferredSize(new Dimension(width,height/2));
+        registerLabel.setHorizontalAlignment(JLabel.CENTER);
+        cardRegister.add(registerLabel);
+        textField = new JTextField(10);
+        textField.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 25));
+        cardRegister.add(textField);
+        buttonOk = new JButton("登録");
+        buttonOk.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 25));
+        buttonOk.addActionListener(this);
+        cardRegister.add(buttonOk);
         //カード１
         card1 = new JPanel();
         card1.setLayout(new GridLayout(1,3));
         button1 = new JButton("グー");
+        button1.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 25));
         button1.addActionListener(this);
         card1.add(button1);
         button2 = new JButton("チョキ");
+        button2.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 25));
         button2.addActionListener(this);
         card1.add(button2);
         button3 = new JButton("パー");
+        button3.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 25));
         button3.addActionListener(this);
         card1.add(button3);
         //カード２
         card2 = new JPanel();
         card2.setLayout(new BorderLayout());
-        handLabel.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 30));
-        handLabel.setPreferredSize(new Dimension(width,height/2));
+        handLabel.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 25));
+        handLabel.setPreferredSize(new Dimension(width,height/3));
         handLabel.setHorizontalAlignment(JLabel.CENTER);
         card2.add("Center",handLabel);
-        judgeLabel.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 40));
+        judgeLabel.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 50));
         judgeLabel.setPreferredSize(new Dimension(width,height/2));
         judgeLabel.setHorizontalAlignment(JLabel.CENTER);
         card2.add("North",judgeLabel);
         againButton = new JButton("もう一回");
+        againButton.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 25));
         againButton.addActionListener(this);
         card2.add("South",againButton);
         //パネルにカードを登録
         cardPanel = new JPanel();
         layout = new CardLayout();
         cardPanel.setLayout(layout);
+        cardPanel.add(cardRegister);
         cardPanel.add(card1);
         cardPanel.add(card2);
 
@@ -66,7 +90,11 @@ public class client extends JFrame implements ActionListener{
     }
     //イベント処理
     public void actionPerformed(ActionEvent ae){
-        if (ae.getSource() == button1){
+        if (ae.getSource() == buttonOk){
+            myhand = 0;
+            name = textField.getText();
+            layout.next(cardPanel);
+        }else if (ae.getSource() == button1){
             myhand = 1;
             layout.next(cardPanel);
         }else if (ae.getSource() == button2){
@@ -77,13 +105,14 @@ public class client extends JFrame implements ActionListener{
             layout.next(cardPanel);
         }else if (ae.getSource() == againButton){
             myhand = 0;
-            layout.first(cardPanel);
+            layout.previous(cardPanel);
         }
         if(myhand==1 || myhand==2 || myhand==3){
             try{
                 Socket socket = new Socket("127.0.0.1",5000);
                 DataOutputStream out = new DataOutputStream(socket.getOutputStream());
                 out.writeInt(myhand);       //選択した手を送信
+                out.writeUTF(name);        //名前送信
                 BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 String message[] = (reader.readLine()).split("/");     //結果を受け取り
                 handLabel.setText(message[0]);     //カード２のラベルに結果を与える
--- src/server.java
+++ src/server.java
@@ -2,19 +2,37 @@ import java.io.*;
 import java.net.*;
 import java.util.Random;
 
-class MyThread extends Thread{
-    public MyThread(Socket socket){
+public class server {
+    static String tempName="";
+    static int temphand=0;
+    public static void main(String[] args){
+        ServerSocket serverSocket;
         try{
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            int cpu = cpu();
-            int client_hand = in.readInt();
-            String judge = judge(client_hand,cpu);
-            PrintWriter writer = new PrintWriter(socket.getOutputStream());
-            final String[] hand = {null,"グー","チョキ","パー"};
-            String message ="YOU("+hand[client_hand]+")   vs   CPU("+hand[cpu]+")/"+judge;
-            writer.println(message);
-            System.out.println(socket.getRemoteSocketAddress()+ " 送信:" +message);
-            writer.close();
+            serverSocket = new ServerSocket(5000);
+            while(true){
+                Socket socket = serverSocket.accept();
+                DataInputStream in = new DataInputStream(socket.getInputStream());
+                String serverName;
+                int server_hand;
+                int hand = in.readInt();
+                String name = in.readUTF();
+                if(name.equals(tempName) || tempName.equals("")){
+                    server_hand = cpu();
+                    serverName = "CPU";
+                }else{
+                    server_hand = temphand;
+                    serverName = tempName;
+                }
+                tempName = name;
+                temphand = hand;
+                String judge = judge(hand,server_hand);
+                PrintWriter writer = new PrintWriter(socket.getOutputStream());
+                final String[] hands = {null,"グー","チョキ","パー"};
+                String message =name+"("+hands[hand]+")  VS  "+serverName+"("+hands[server_hand]+")/"+judge;
+                writer.println(message);
+                System.out.println(socket.getRemoteSocketAddress()+ " 送信:" +message);
+                writer.close();
+            }
         }catch(IOException e){
             System.out.println(e);
         }
@@ -24,29 +42,13 @@ class MyThread extends Thread{
         int server_hand = random.nextInt(3)+1;
         return server_hand;
     }
-    private static String judge(int client_hand,int server_hand){
-        if (client_hand == server_hand){
+    private static String judge(int hand,int server_hand){
+        if (hand == server_hand){
             return "引き分け";
-        }else if((client_hand-server_hand+3)%3 == 2){
+        }else if((hand-server_hand+3)%3 == 2){
             return "勝ち";
         }else{
             return "負け";
-        }
-    }
-}
-
-public class server {
-    public static void main(String[] args){
-        ServerSocket serverSocket;
-        try{
-            serverSocket = new ServerSocket(5000);
-            while(true){
-                Socket socket = serverSocket.accept();
-                MyThread thread = new MyThread(socket);
-                thread.start();
-            }
-        }catch (IOException e){
-            System.out.println(e);
         }
     }
 }
